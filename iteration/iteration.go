1894f039df56c04bd33dc60f1f8bf7b7461ed33b
Barrett <barrettkowalsky@gmail.com>
2025-12-07 12:04:30 -0700

    more iteration iterations

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,12 +1,13 @@
 package iteration
 
-import "strings"
+import (
+	"strings"
+)
 
-const repeatCount = 5
-
-func Repeat(char string) string {
+func Repeat(char string, count int) string {
 	var repeated strings.Builder
-	for i := 0; i < repeatCount; i++ {
+
+	for i := 0; i < count; i++ {
 		repeated.WriteString(char)
 	}
 	return repeated.String()
--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,18 +1,43 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestIterate(t *testing.T) {
-	iteration := Repeat("a")
-	expected := "aaaaa"
+func TestRepeat(t *testing.T) {
+	t.Run("repeat the standard five times", func(t *testing.T) {
+		got := Repeat("a", 5)
+		expected := "aaaaa"
 
-	if iteration != expected {
-		t.Errorf("expected %q but got %q", expected, iteration)
-	}
+		if got != expected {
+			t.Errorf("expected %q but got %q", expected, got)
+		}
+	})
+	t.Run("repeat a custom number of times", func(t *testing.T) {
+		got := Repeat("a", 12)
+		expected := "aaaaaaaaaaaa"
+
+		if got != expected {
+			t.Errorf("expected %q but got %q", expected, got)
+		}
+	})
+}
+
+// TODO: assetCorrectMessage
+
+// TODO: Handle negative numbers
+
+// TODO: Handle uppercase letters
+
+func ExampleRepeat() {
+	iterationSample := Repeat("r", 7)
+	fmt.Println(iterationSample)
+	// Output: rrrrrrr
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for b.Loop() {
-		Repeat("a")
+		Repeat("a", 6)
 	}
 }
